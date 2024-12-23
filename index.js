ef4c368a4f356cb3f0fd33eb4a91cb935e5fec9b
Fahad Ali Babar <rajafahadali931@gmail.com>
2024-12-23 20:29:52 +0500

    jwt authentication

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ mongoose.connect(database)
     console.log("connected to database");
 })
 .catch((err)=>{
-    console.log("Not connected to database",err);
+    console.log("Not connecteddd to database",err);
 })
 app.get("/",(req,res)=>{
     res.send("hellooo!")
